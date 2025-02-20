94c213e080c378d5ce2bc567e53d74bedb622e4a
Manoj Hortulanus <mdhortulanus@gmail.com>
2025-02-20 10:25:30 +0100

    wip

--- src/Resources/LanguageResource/Pages/CreateLanguage.php
+++ src/Resources/LanguageResource/Pages/CreateLanguage.php
@@ -3,7 +3,7 @@
 namespace Backstage\Translations\Filament\Resources\LanguageResource\Pages;
 
 use Backstage\Translations\Filament\Resources\LanguageResource;
-use Backstage\Translations\Laravel\Jobs\ScanTranslationStrings;
+use Backstage\Translations\Laravel\Jobs\TranslateKeys;
 use Filament\Resources\Pages\CreateRecord;
 use Illuminate\Contracts\Support\Htmlable;
 
@@ -11,13 +11,33 @@ class CreateLanguage extends CreateRecord
 {
     protected static string $resource = LanguageResource::class;
 
+    protected $langRequiresTranslation = false;
+
     public function getTitle(): string | Htmlable
     {
         return __('Create Language');
     }
 
+    public function beforeCreate()
+    {
+        $this->langRequiresTranslation = $this->data['translate_after_creation'];
+    }
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        unset($data['translate_after_creation']);
+
+        return $data;
+    }
+
+
     protected function afterCreate()
     {
+        if ($this->langRequiresTranslation) {
+            dispatch(new TranslateKeys($this->record))
+                ->delay(now()->addSeconds(30));
+        }
+
         $this->redirect(EditLanguage::getUrl(['record' => $this->record->code]));
     }
 }
