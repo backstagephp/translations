4c959a673a66364cedfe15b824e58758893f4b78
Mark van Eijk <mark@vaneijk.co>
2025-02-21 15:42:47 +0100

    wip

--- src/helpers.php
+++ src/helpers.php
@@ -2,6 +2,17 @@
 
 use Backstage\Translations\Filament\TranslationsPlugin;
 
+function getCountryFlag($code)
+{
+    $code = explode('-', $code)[0];
+
+    if ($code == 'en') {
+        return 'flag-country-us';
+    }
+
+    return 'flag-country-' . $code;
+}
+
 function filamentTranslations(): TranslationsPlugin
 {
     return TranslationsPlugin::get();
