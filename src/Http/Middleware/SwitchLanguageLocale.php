cfc9df8ffcb0c35a8d269781d12d26f27287c4b8
Mark van Eijk <mark@vormkracht10.nl>
2025-02-21 14:38:10 +0100

    Update SwitchLanguageLocale.php

--- src/Http/Middleware/SwitchLanguageLocale.php
+++ src/Http/Middleware/SwitchLanguageLocale.php
@@ -14,16 +14,8 @@ class SwitchLanguageLocale
             $preferredLocale = LanguageResource::getModel()::where('default', true)?->first();
 
             if (! $preferredLocale) {
-                config(['languages.code' => 'en_US']);
-                config(['languages.language_code' => 'en']);
-
-                app()->setLocale(locale: 'en');
-
                 return $next($request);
             }
-
-            config(['languages.code' => $preferredLocale->code]);
-            config(['languages.language_code' => $preferredLocale->languageCode]);
 
             app()->setLocale(
                 locale: $preferredLocale->languageCode
@@ -32,11 +24,10 @@ class SwitchLanguageLocale
             return $next($request);
         }
 
-        $preferredLocale = session()->get('languages')['language_code'] ??
-            request()->get('locale') ??
-            request()->cookie('filament_language_switch_locale') ??
-            config('app.locale', 'en') ??
-            request()->getPreferredLanguage();
+        $preferredLocale = session('locale') ?:
+            request()->get('locale') ?:
+            LanguageResource::getModel()::where('code', str_replace('_', '-', request()->getPreferredLanguage()))->first()?->languageCode ?:
+            config('app.locale', 'en');
 
         app()->setLocale(
             locale: $preferredLocale
