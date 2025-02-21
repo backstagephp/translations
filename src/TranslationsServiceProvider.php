7356507a9a3b852fcb051de76a47461d7ee429c7
Mark van Eijk <mark@vaneijk.co>
2025-02-21 16:18:20 +0100

    blade flags

--- src/TranslationsServiceProvider.php
+++ src/TranslationsServiceProvider.php
@@ -2,14 +2,18 @@
 
 namespace Backstage\Translations\Filament;
 
-use Backstage\Translations\Filament\Testing\TestsFilamentTranslations;
+use BladeUI\Icons\Factory;
+use Livewire\Livewire;
 use Filament\Support\Assets\Css;
+use Spatie\LaravelPackageTools\Package;
+use Filament\Support\Facades\FilamentIcon;
+use Illuminate\Contracts\Container\Container;
+use Illuminate\Support\Facades\Blade;
 use Filament\Support\Facades\FilamentAsset;
 use Livewire\Features\SupportTesting\Testable;
-use Livewire\Livewire;
-use Spatie\LaravelPackageTools\Commands\InstallCommand;
-use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
+use Spatie\LaravelPackageTools\Commands\InstallCommand;
+use Backstage\Translations\Filament\Testing\TestsFilamentTranslations;
 
 class TranslationsServiceProvider extends PackageServiceProvider
 {
@@ -55,22 +59,43 @@ class TranslationsServiceProvider extends PackageServiceProvider
             $this->getAssetPackageName()
         );
 
+        FilamentIcon::register($this->getIcons());
+
         Testable::mixin(new TestsFilamentTranslations);
 
         require_once __DIR__ . '/helpers.php';
 
         Livewire::component('backstage-translations::switcher', \Backstage\Translations\Filament\Components\Switcher::class);
     }
+    
+    public function packageRegistered()
+    {
+        $this->callAfterResolving(Factory::class, function (Factory $factory, Container $container) {
+            $config = $container->make('config')->get('blade-flags', [
+                'prefix' => 'flag',
+            ]);
+
+            $factory->add('blade-flags', array_merge(['path' => __DIR__ . '/../resources/svg'], $config));
+        });
+    }
 
     protected function getAssetPackageName(): ?string
     {
-        return 'backstagephp/translations';
+        return 'backstage/translations';
     }
 
     protected function getAssets(): array
     {
         return [
             Css::make('filament-progress-column-styles', __DIR__ . '/../resources/dist/filament-progress-column.css'),
+        ];
+    }
+    
+    protected function getIcons(): array
+    {
+        return [
+            Blade::aliasComponent('flag-country-us', 'flag_country_en'),
+            Blade::aliasComponent('flag-country-en', 'flag_country_en'),
         ];
     }
 
