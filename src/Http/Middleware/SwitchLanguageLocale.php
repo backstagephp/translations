378f5ba503a1ebbe419d4b5f1d9c17cbec519cc4
markvaneijk <1925388+markvaneijk@users.noreply.github.com>
2025-02-25 09:27:22 +0000

    Fix styling

--- src/Http/Middleware/SwitchLanguageLocale.php
+++ src/Http/Middleware/SwitchLanguageLocale.php
@@ -20,9 +20,9 @@ class SwitchLanguageLocale
             app()->setLocale(
                 locale: $preferredLocale->languageCode
             );
-    
+
             session(['language' => $preferredLocale->only('code', 'name', 'native', 'localizedLanguageName', 'localizedCountryName')]);
-            
+
             view()->share('preferredLocale', $preferredLocale);
 
             return $next($request);
@@ -32,7 +32,7 @@ class SwitchLanguageLocale
             LanguageResource::getModel()::where('code', str_replace('_', '-', (string) request()->getPreferredLanguage()))->first() ?:
             LanguageResource::getModel()::default();
 
-        if($preferredLocale) {
+        if ($preferredLocale) {
             session(['language' => $preferredLocale->only('code', 'name', 'native', 'localizedLanguageName', 'localizedCountryName')]);
 
             app()->setLocale(
