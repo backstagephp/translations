f3faafd1e77cadd16151075c8423caf823804d0f
arduinomaster22 <91618246+arduinomaster22@users.noreply.github.com>
2025-02-27 14:48:55 +0000

    Fix styling

--- src/Resources/TranslationResource/Pages/ListTranslations.php
+++ src/Resources/TranslationResource/Pages/ListTranslations.php
@@ -2,7 +2,6 @@
 
 namespace Backstage\Translations\Filament\Resources\TranslationResource\Pages;
 
-use Backstage\Translations\Filament\Resources\LanguageResource;
 use Backstage\Translations\Filament\Resources\LanguageResource\Pages\CreateLanguage;
 use Backstage\Translations\Filament\Resources\TranslationResource;
 use Backstage\Translations\Laravel\Jobs\ScanTranslationStrings;
