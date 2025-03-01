29c244dee3ecbae2f415d0897d1298a385e8afc0
Manoj Hortulanus <mdhortulanus@gmail.com>
2025-03-01 15:46:51 +0100

    wip

--- src/Resources/LanguageResource/Pages/CreateLanguage.php
+++ src/Resources/LanguageResource/Pages/CreateLanguage.php
@@ -11,32 +11,13 @@ class CreateLanguage extends CreateRecord
 {
     protected static string $resource = LanguageResource::class;
 
-    protected $langRequiresTranslation = false;
-
     public function getTitle(): string | Htmlable
     {
         return __('Create Language');
     }
 
-    public function beforeCreate()
-    {
-        $this->langRequiresTranslation = $this->data['translate_after_creation'];
-    }
-
-    protected function mutateFormDataBeforeCreate(array $data): array
-    {
-        unset($data['translate_after_creation']);
-
-        return $data;
-    }
-
     protected function afterCreate()
     {
-        if ($this->langRequiresTranslation) {
-            dispatch(new TranslateKeys($this->record))
-                ->delay(now()->addSeconds(30));
-        }
-
         $this->redirect(EditLanguage::getUrl(['record' => $this->record->code]));
     }
 }
